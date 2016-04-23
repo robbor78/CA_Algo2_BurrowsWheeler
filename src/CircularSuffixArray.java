4451c1019fbb40b6f499fa226c5045e72a6ccd1d
name <email>
2016-04-23 19:25:17 +0200

    Added move to front. Added guards to circular.

--- src/CircularSuffixArray.java
+++ src/CircularSuffixArray.java
@@ -1,7 +1,11 @@
 public class CircularSuffixArray {
+
+    private final int N;
+
     // circular suffix array of s
     public CircularSuffixArray(String s) {
-
+        CheckNullOrEmpty(s);
+        N = s.length();
     }
 
     // length of s
@@ -11,7 +15,20 @@ public class CircularSuffixArray {
 
     // returns index of ith sorted suffix
     public int index(int i) {
+        CheckIndex(i);
         return -1;
+    }
+
+    private void CheckNullOrEmpty(String s) {
+        if (s == null || s.trim().length() == 0) {
+            throw new java.lang.NullPointerException();
+        }
+    }
+
+    private void CheckIndex(int index) {
+        if (index < 0 || index >= N) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
     }
 
     // unit testing of the methods
