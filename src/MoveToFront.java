12997bd05fbe03df4559c106342f22a42e77d6f8
name <email>
2016-04-24 22:46:49 +0200

    move to front

--- src/MoveToFront.java
+++ src/MoveToFront.java
@@ -4,24 +4,35 @@ import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
 
 public class MoveToFront {
+
+    private static final int RADIX = 256;
+    private static final int BITS = 8;
+
     // apply move-to-front encoding, reading from standard input and writing to
     // standard output
     public static void encode() {
-        int[] seq = IntStream.range(0, 255).toArray();
+        int[] seq = IntStream.range(0, RADIX).toArray();
+        // System.out.println(seq.length);
         while (!BinaryStdIn.isEmpty()) {
-            int c = BinaryStdIn.readInt(8);
-            
+            int c = BinaryStdIn.readInt(BITS);
+
             int value = seq[c];
-            
-            BinaryStdOut.write(value, 8);
-            
+
+            // System.out.printf("0x%04X -> 0x%04X\n", c, value);
+
+            BinaryStdOut.write(value, BITS);
+
+            // if (value != 0) {
             seq[c] = 0;
-            
-            for (int i=0; i<c; i++) {
-                seq[i]++;
+
+            for (int i = 0; i < RADIX; i++) {
+                if (i != c && seq[i] < value) {
+                    seq[i]++;
+                }
             }
+            // }
         }
-        
+
         BinaryStdOut.flush();
         BinaryStdOut.close();
     }
@@ -29,7 +40,22 @@ public class MoveToFront {
     // apply move-to-front decoding, reading from standard input and writing to
     // standard output
     public static void decode() {
+        int[] seq = IntStream.range(0, RADIX).toArray();
+        while (!BinaryStdIn.isEmpty()) {
+            int i = BinaryStdIn.readInt(BITS);
 
+            int c = seq[i];
+
+            BinaryStdOut.write((char) c);
+
+            for (int k = i; k > 0; k--) {
+                seq[k] = seq[k-1];
+            }
+            seq[0] = c;
+
+        }
+        BinaryStdOut.flush();
+        BinaryStdOut.close();
     }
 
     // if args[0] is '-', apply move-to-front encoding
