9ee5556cc4a37090a1b7d40fe45ce8e9d2c9d6e8
name <email>
2016-04-26 23:36:28 +0200

    Working but slow.

--- src/BurrowsWheeler.java
+++ src/BurrowsWheeler.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
@@ -27,6 +28,8 @@ public class BurrowsWheeler {
         for (i = 0; i < length; i++) {
             int index = csa.index(i);
             char c = in.charAt((index + length - 1) % length);
+            // System.out.println(in.substring(index) + in.substring(0, index) +
+            // " " + c);
             // char c = in.charAt(csa.index(i));
             // System.out.println(c+" "+csa.index(i));
             BinaryStdOut.write(c);
@@ -39,8 +42,7 @@ public class BurrowsWheeler {
     // apply Burrows-Wheeler decoding, reading from standard input and writing
     // to standard output
     public static void decode() {
-        // int first =
-        BinaryStdIn.readInt();
+        int first = BinaryStdIn.readInt();
         // System.out.println("1st: " + first);
 
         ArrayList<Integer> al = new ArrayList<Integer>();
@@ -53,17 +55,21 @@ public class BurrowsWheeler {
 
         int length = al.size();
         Integer[] t = new Integer[length];
+        Integer[] sorted = new Integer[length];
         t = al.toArray(t);
+        sorted = al.toArray(sorted);
+
+        Arrays.sort(sorted);
 
         int[] freq = new int[256 + 1];
 
-        for (int i = 0; i < length; i++) {
-            freq[t[i] + 1]++;
-        }
+        // for (int i = 0; i < length; i++) {
+        // freq[t[i] + 1]++;
+        // }
 
-        for (int i = 0; i < 256; i++) {
-            freq[i + 1] += freq[i]; // cumulates
-        }
+        // for (int i = 0; i < 256; i++) {
+        // freq[i + 1] += freq[i]; // cumulates
+        // }
 
         // for (int i = 0; i < length; i++) {
         // System.out.println(t[i] + "->" + freq[t[i]]);
@@ -71,25 +77,41 @@ public class BurrowsWheeler {
 
         int[] next = new int[length];
 
+        // for (int i = 0; i < length; i++) {
+        // freq[t[i]]++;
+        // next[i] = freq[t[i]];
+        // System.out.println("n(" + i + "): " + next[i]);
+        // }
+
         for (int i = 0; i < length; i++) {
-            freq[t[i]]++;
-            next[i] = freq[t[i]];
-            // System.out.println(next[i]);
+            int sortedC = sorted[i];
+            int k = freq[sortedC];
+            for (; k < length; k++) {
+                if (t[k] == sortedC) {
+                    break;
+                }
+            }
+            next[i] = k;
+            freq[sortedC] = k + 1;
+            // System.out.println("n(" + i + "): " + next[i]);
         }
 
-        StringBuilder sb = new StringBuilder();
-        int r = 1;
-        for (int i = length - 2; i >= 0; i--) {
+        //StringBuilder sb = new StringBuilder();
+        int r = first;
+        for (int i = 0; i < length; i++) {
             // System.out.println("r= "+r);
-            int c = t[r - 1];
-            sb.insert(0, (char) c);
-            r = next[r - 1];
+            // int c = t[r - 1];
+            int c = sorted[r];
+            // sb.insert(0, (char) c);
+            // sb.append((char) c);
+            BinaryStdOut.write((char) c);
+            r = next[r];
         }
-        sb.append((char) (int) t[r - 1]);
+        // sb.append((char) (int) t[r - 1]);
 
         // System.out.println(sb.toString());
 
-        BinaryStdOut.write(sb.toString());
+        //BinaryStdOut.write(sb.toString());
         BinaryStdOut.flush();
         BinaryStdOut.close();
 
